5a2cf1b3faa1c592d52200b2a5150db20bceea75
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-02-09 14:53:35 +0100

    plik json już jest

--- jsonmapperParent/src/main/java/pl/sda/mapper/Main.java
+++ jsonmapperParent/src/main/java/pl/sda/mapper/Main.java
@@ -1,7 +1,10 @@
 package pl.sda.mapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import pl.sda.lambda.Movie;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -14,19 +17,29 @@ public class Main {
         List<Movie> movies = Arrays.asList(
                 new Movie("Tytul",
                         "Jan",
-                        LocalDate.of(2012,02,12),
+                        LocalDate.of(2012, 02, 12),
                         60,
                         Arrays.asList("Jan", "Maciek")),
                 new Movie("Tytul2",
                         "Jan",
-                        LocalDate.of(2012,02,12),
+                        LocalDate.of(2012, 02, 12),
                         20,
                         Arrays.asList("Michal", "Maciek")),
                 new Movie("Tytul3",
                         "Kamil",
-                        LocalDate.of(2005, 05,30),
+                        LocalDate.of(2005, 05, 30),
                         33,
                         Arrays.asList("Jan", "Maciek"))
         );
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        File file = new File("movies.json");
+
+        try {
+            mapper.writeValue(file, movies);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
